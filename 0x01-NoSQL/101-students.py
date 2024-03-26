31e7f1e75c880ddb82baf00f6b84b385ca22de18
BelalGhonaim <belalpop1999@yahoo.com>
2024-03-26 23:17:22 +0200

    add first 2 files in aDvance tasks

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""function that returns all students sorted by average score """
+
+
+def top_students(mongo_collection):
+    """function that returns all students sorted by average score """
+    return mongo_collection.aggregate([
+        {"$unwind": "$topics"},
+        {"$group": {
+            "_id": "$_id",
+            "name": {"$first": "$name"},
+            "averageScore": {"$avg": "$topics.score"}
+        }},
+        {"$sort": {"averageScore": -1}}
+    ])
