b0f7191c96d105617452295034e1929190b6479a
BelalGhonaim <belalpop1999@yahoo.com>
2024-05-27 22:49:53 +0300

    updated file 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
 --SQL Script that creates a table users
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL
 );
