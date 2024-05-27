5d86ec39e00a07de2b48f41ae7c80ccbdaa5a846
BelalGhonaim <belalpop1999@yahoo.com>
2024-05-27 22:46:50 +0300

    add id serial

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 --SQL Script that creates a table users
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
