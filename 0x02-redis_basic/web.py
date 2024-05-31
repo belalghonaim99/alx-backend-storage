69c4a5286d37fa56a24f8b1fbbbfb437266243d3
BelalGhonaim <belalpop1999@yahoo.com>
2024-05-31 16:52:18 +0300

    testing

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -14,17 +14,19 @@ def data_cacher(method: Callable) -> Callable:
     """Decorator that caches the output of a function."""
     @wraps(method)
     def invoker(url) -> str:
-        """ Invoker function that caches the output of fetched data.
-            Increment the access count for the URL"
-        """
+        """ Invoker function that caches the output of fetched data."""
+        # Increment the access count for the URL
         redis_store.incr(f'count:{url}')
+        
+        # Check if the result is already cached
         result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
+
         result = method(url)
         redis_store.setex(f'result:{url}', 10, result)
         return result
-    
+
     return invoker
 
 
@@ -35,8 +37,10 @@ def get_page(url: str) -> str:
     """
     return requests.get(url).text
 
-"""Used to simulate a slow response and test your caching."""
+
 if __name__ == "__main__":
     url = 'http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.google.com'
-    print(get_page(url))
-    print(get_page(url))
+    print("Accessing URL:", url)
+    print("Content:", get_page(url))
+    
+    print("Access Count for", url, ":", redis_store.get(f'count:{url}').decode('utf-8'))
