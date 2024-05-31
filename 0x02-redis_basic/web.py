ab5e8763c9f6b76f919a246c05abd88bf8088c5b
BelalGhonaim <belalpop1999@yahoo.com>
2024-05-31 16:59:38 +0300

    updated pycode

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,37 +6,27 @@ from functools import wraps
 from typing import Callable
 
 
-redis_store = redis.Redis()
-""" Redis store instance."""
+store = redis.Redis()
+""" Redis instance for caching and tracking requests."""
 
 
 def data_cacher(method: Callable) -> Callable:
-    """Decorator that caches the output of a function."""
+    """ Decorator that caches the output of a method."""
     @wraps(method)
     def invoker(url) -> str:
-        """ Invoker function that caches the output of fetched data.
-            Increment the access count for the URL"
-        """
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-
+        """ Invoker function that caches the output of fetched data."""
+        store.incr(f'count:{url}')
+        result_redis = store.get(f'result:{url}')
+        if result_redis:
+            return result_redis.decode('utf-8')
+        result_redis = method(url)
+        store.set(f'count:{url}', 0)
+        store.setex(f'result:{url}', 10, result_redis)
+        return result_redis
     return invoker
 
 
 @data_cacher
 def get_page(url: str) -> str:
-    """ Fetches the page content
-    From HTML FILE
-    """
+    """ Fetches the content of a web page."""
     return requests.get(url).text
-
-
-"""Used to simulate a slow response and test your caching."""
-
-
-if __name__ == "__main__":
